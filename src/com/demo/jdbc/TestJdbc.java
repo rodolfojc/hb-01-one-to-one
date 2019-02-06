df80387f474017767ee49dd6629222a774cfb050
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2019-02-06 23:42:22 +0000

    Copy and Renaming
    New project - Cloned

--- src/com/demo/jdbc/TestJdbc.java
+++ src/com/demo/jdbc/TestJdbc.java
@@ -7,7 +7,7 @@ public class TestJdbc {
 
 	public static void main(String[] args) {
 		
-		String jdbcurl = "jdbc:mysql://localhost:3306/hb_student_tracker?useSSL=false";
+		String jdbcurl = "jdbc:mysql://localhost:3306/hb-01-one-to-one-uni?useSSL=false";
 		String user = "hbstudent";
 		String pass = "hbstudent";
 		try {
