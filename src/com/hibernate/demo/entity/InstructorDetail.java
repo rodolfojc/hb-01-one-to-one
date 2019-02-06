ce6861059eefb09bf93f759ee6d47ea470cbe609
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2019-02-06 23:51:47 +0000

    Update InstructorDetail.java

--- src/com/hibernate/demo/entity/InstructorDetail.java
+++ src/com/hibernate/demo/entity/InstructorDetail.java
@@ -23,5 +23,43 @@ public class InstructorDetail {
 	@Column(name="hobby")
 	private String hobby;
 	
+	public InstructorDetail() {
 		
+	}
+
+	public InstructorDetail(String youtube_channel, String hobby) {
+		this.youtube_channel = youtube_channel;
+		this.hobby = hobby;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getYoutube_channel() {
+		return youtube_channel;
+	}
+
+	public void setYoutube_channel(String youtube_channel) {
+		this.youtube_channel = youtube_channel;
+	}
+
+	public String getHobby() {
+		return hobby;
+	}
+
+	public void setHobby(String hobby) {
+		this.hobby = hobby;
+	}
+
+	@Override
+	public String toString() {
+		return "InstructorDetail [id=" + id + ", youtube_channel=" + youtube_channel + ", hobby=" + hobby + "]";
+	}
+	
+	
 }
